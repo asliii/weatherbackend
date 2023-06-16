68897f5dc9e429f6edf206d34e7261ff9fb0de69
Aslıhan Yiğit <aslinagi.95@outlook.com>
2023-06-16 14:47:16 +0300

    fix

---
+++ weatherApp/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated, AllowAny  # NOQA
+from . import serializers
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.none()
+    serializer_class = serializers.UserSerializer
+    permission_classes = (IsAuthenticated,)
+    http_method_names = ['get', 'post']
+
+    @action(detail=False, methods=["post"], url_path=r'user_detail',)
+    def user_detail(self):
+        print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+        instance = self.get_object()
+        # query = request.GET.get('query', None)  # read extra data
+        return Response(self.serializer_class(instance).data,
+                        status=status.HTTP_200_OK)
